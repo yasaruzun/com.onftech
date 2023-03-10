f3b9b955ba32132df06cbbc00997da66ddebf6bd
Yasar UZUN <116967168+yasaruzun@users.noreply.github.com>
2023-03-10 20:47:51 +0300

    yasar 10.03.2023

--- src/test/java/stepDefinitions/HomePageStepDefinitions.java
+++ src/test/java/stepDefinitions/HomePageStepDefinitions.java
@@ -1,8 +1,35 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import pages.HomePage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class HomePageStepDefinitions {
 
 
+    HomePage homePage=new HomePage();
+    @Given("go to the {string}")
+    public void go_to_the(String istenenUrl) {
+        homePage=new HomePage();
+        Driver.getDriver().get(ConfigReader.getProperty(istenenUrl));
+
+    }
+    @Then("test that the url contains {string}")
+    public void testThatTheUrlContains(String istenenkelime) {
+        homePage=new HomePage();
+        String actualUrl= Driver.getDriver().getCurrentUrl();
+        Assert.assertTrue(actualUrl.contains(istenenkelime));
+
+    }
+
+    @Then("sayfayi kapat")
+    public void sayfayiKapat() {
+        homePage=new HomePage();
+        Driver.closeDriver();
 
 
+    }
 }
--- src/test/resources/features/US_001/TC_001.feature
+++ src/test/resources/features/US_001/TC_001.feature
@@ -1,5 +1,9 @@
-Feature:
+Feature:US_001 Home page must be accessed by a user
 
-  Scenario:
+  Scenario:TC_001 The home page must be accessible when the URL is entered
+
+    Given go to the "userUrl"
+    Then  test that the url contains "trendlifebuy"
+    Then  sayfayi kapat
 
 
